f11d57004918c182a02d51c8b1c1397f67c10c02
siggismara <runningman69@gmail.com>
2017-08-24 15:36:17 +0200

    cleanup of project tests

--- test/test_model/test_project.py
+++ test/test_model/test_project.py
@@ -7,16 +7,16 @@ import mrmProcessing.model.project as project
 
 
 class testProject(unittest.TestCase):
-    def test_init01(self):
+    def test_001_init(self):
         """Tests if not having any parameters in the init call raises a TypeError"""
         self.assertRaises(TypeError,project.project)
-    def test_init02(self):
+    def test_002_init(self):
         """Tests if having the id parameters generates a new object of type project"""
         self.assertIsInstance(project.project("id"),project.project)
-    def test_init03(self):
+    def test_003_init(self):
         """Tests if having the named parameter "name" generates a new object of type project"""
         self.assertIsInstance(project.project("id",name="name"),project.project)
-    def test_init03(self):
+    def test_004_init(self):
         """Tests if having the named parameter "basePath" generates a new object of type project"""
         self.assertIsInstance(project.project("id",basePath="basePath"),project.project)
 
