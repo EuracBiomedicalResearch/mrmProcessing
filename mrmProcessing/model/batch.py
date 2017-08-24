966b07af88f107063478660674b811517ee89cf6
siggismara <runningman69@gmail.com>
2017-08-24 15:35:50 +0200

    init batch tests done
    Including some fixes in the batch class

--- mrmProcessing/model/batch.py
+++ mrmProcessing/model/batch.py
@@ -3,25 +3,31 @@
 
 from copy import deepcopy
 import weakref
+import glob
 
+from dataHolders import xyDataHolder
 import rt
 import sample
+import rawdata
 
 class batch():
 
-    def __init__(self,myid,project,name="",dataPath="",projIniRtList=list()):
+    def __init__(self,myid,project,name="",dataPath="",iniRtList=list()):
         self.myid=myid
         self.project=weakref.proxy(project)
         self.name=name
         self.dataPath=dataPath
-        self.rtList=deepcopy(projIniRtList)
+        self.rtList=deepcopy(iniRtList)
         self.sampleList=list()
+        self.rawdataList=list()
 
     def newRT(self,rtID,ionchrom,rtFrom=0.0,rtTo=0.0):
         self.rtList.append(rt(rtID,ionchrom,self,rtFrom,rtTo))
     def newSample(self,samID, samplName="",samplID="",sampleType="unknown"):
         self.sampleList.append(sample(samID,self,samplName,samplID,sampleType))
-
+    def newRawdata(self,myid,ionchrom,sample,anxyDataHolder=xyDataHolder("")):
+        #note: ionchrom here comes from iniRtList as it already connects rt and mz1/mz2 and the processing parameters
+        self.rawdataList.append(myid,ionchrom,sample,anxyDataHolder)
     def readDataPath(self):
         #glob mzML from dataPath
         #either read the mzML files and add samples and rawdata directly
--- test/test_model/test_batch.py
+++ test/test_model/test_batch.py
@@ -8,15 +8,16 @@ import mrmProcessing.model.batch as batch
 
 
 class testBatch(unittest.TestCase):
-    def test_init01(self):
+    def test_001_init(self):
         """Tests if not having any parameters in the init call raises a TypeError"""
         self.assertRaises(TypeError,batch.batch)
-    def test_init02(self):
+    def test_002_init(self):
         """Tests if having only the id parameter raises a TypeError"""
         self.assertRaises(TypeError,batch.batch,"id")
-    def test_init03(self):
+    def test_003_init(self):
         """Tests if having both the id and project parameter generates a new object of type batch"""
         self.assertIsInstance(batch.batch("id",project.project("projID")),batch.batch)
+    def test_004_init(self):
         """Tests if project parameter of the batch class returns the right name"""
         proj=project.project("projID")
         bat=batch.batch("id",proj)
