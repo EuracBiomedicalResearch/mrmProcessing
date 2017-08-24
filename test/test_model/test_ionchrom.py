d095ed4838ae9c8119e8273635b03d815533364f
siggismara <runningman69@gmail.com>
2017-08-24 16:23:38 +0200

    cleanup of the ionchrom test

--- test/test_model/test_ionchrom.py
+++ test/test_model/test_ionchrom.py
@@ -12,14 +12,6 @@ class testBatch(unittest.TestCase):
     def test_002_init(self):
         """Tests if passing the id parameter generates a new object of type ionchrom"""
         self.assertIsInstance(ionchrom.ionchrom("id"),ionchrom.ionchrom)
-    # def test_003_init(self):
-    #     """Tests if having both the id and project parameter generates a new object of type batch"""
-    #     self.assertIsInstance(ionchrom.ionchrom("id",project.project("projID")),batch.batch)
-    # def test_004_init(self):
-    #     """Tests if project parameter of the batch class returns the right name"""
-    #     proj=project.project("projID")
-    #     bat=batch.batch("id",proj)
-    #     self.assertTrue(bat.project.myid==proj.myid)
-
+    
 if __name__ == '__main__':
     unittest.main()
